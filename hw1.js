9ef4b6d396591cb77579c7140a880529b72d386b
Keyshaonline <keyshaleonidas01@gmail.com>
2021-03-02 09:32:41 -0700

    innitial

---
+++ hw1.js
@@ -0,0 +1,9 @@
+const taco = {id:1, name:'chicken', price:20, about:'Yummy'}
+
+const taco1 = {id:2, name:'carne asada', price:18, about:'Tasty'}
+
+const taco2 = {id:3, name:'fish', price:30, about:'So good'}
+
+
+
+const tacos = [taco, taco1, taco2]
